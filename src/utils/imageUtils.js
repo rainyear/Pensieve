f6bf0869a50b008708bb58c5ee13be848f3ac427
rainyear <sniky.hack@gmail.com>
2020-12-07 23:20:49 +0800

    color theme and pHash

--- src/utils/imageUtils.js
+++ src/utils/imageUtils.js
@@ -1,7 +1,9 @@
 
 import { remote, app } from 'electron'
+import { pHash, SAMPLE_SIZE } from '../utils/pHash'
 
 const crypto = require('crypto')
+const skmeans = require('skmeans')
 const fs = require('fs')
 const Path = require('path')
 const CV = require('opencv4nodejs')
@@ -13,6 +15,9 @@ if (!fs.existsSync(THUMB_PATH)) {
 console.log('OpenCV Version: ', CV.version)
 const THUMB_HEIGHT = 200
 
+function flatten(arr) {
+  return [].concat.apply([], arr)
+}
 function processImage(path, cbk) {
   const name = Path.basename(path)
   const extn = Path.extname(path)
@@ -23,7 +28,18 @@ function processImage(path, cbk) {
   const scale = THUMB_HEIGHT / img.rows
   const thumb = img.rescale(scale)
 
+  // Theme Color
+  // const imgMatFlat = [].concat.apply([], thumb.getDataAsArray())
+  const result = skmeans(flatten(thumb.getDataAsArray()), 10)
+  const themeColors = result.centroids.map(sub => {
+    return sub.map(x => parseInt(x))
+  })
+
   // Hash filename - prevent same image name from different folder
+  const pHashData = thumb.resize(SAMPLE_SIZE, SAMPLE_SIZE).cvtColor(CV.COLOR_BGR2GRAY).getDataAsArray()
+  const hash = pHash(flatten(pHashData))
+
+  // Save Thumbnail
   const saveToName = crypto.createHmac('sha256', 'Pensieve').update(path).digest('hex')
   const saveTo = Path.join(THUMB_PATH, `${saveToName}.${extn}`)
 
@@ -33,7 +49,9 @@ function processImage(path, cbk) {
     thumb_width: parseInt(img.cols * scale),
     thumb_path: saveTo,
     width: img.cols,
-    height: img.rows
+    height: img.rows,
+    themeColors: themeColors,
+    phash: hash
   })
 }
 
